6f115c6379e72fa1e96e103d7e9d22ffac152397
SWEMoHassan <q.and.d@outlook.com>
2023-10-11 16:35:17 -0700

    P

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The integer to compute the absolute value for
+ *
+ * Return: Absolute value of n
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-
-int _putchar(char c);
-int print_sign(int n);
+int _abs(int n);
 
 #endif
