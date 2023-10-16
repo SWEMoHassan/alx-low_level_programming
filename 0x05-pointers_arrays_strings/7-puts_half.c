dd7196943efaad070e8325171962457385f11c34
SWEMoHassan <q.and.d@outlook.com>
2023-10-16 14:58:30 -0700

    :

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,41 +5,21 @@
  * @s: pointer to a string
  * Return: length of the string
  */
-int _strlen(char *s)
-{
-int length = 0;
-
-while (*s != '\0')
-{
-length++;
-s++;
-}
-
-return (length);
-}
-
-/**
- * puts_half - prints half of a string, followed by a new line
- * @str: pointer to the string
- */
 void puts_half(char *str)
 {
-int length = _strlen(str);
-int i, start;
+int length = 0;
+int halfLength;
+int i;
+while (str[length] != '\0')
+length++;
 
 if (length % 2 == 0)
-{
-start = length / 2;
-}
+halfLength = length / 2;
 else
-{
-start = (length - 1) / 2;
-}
-for (i = start; i < length; i++)
-{
+halfLength = (length - 1) / 2;
+
+for (i = halfLength; i < length; i++)
 _putchar(str[i]);
-}
 
 _putchar('\n');
 }
-
