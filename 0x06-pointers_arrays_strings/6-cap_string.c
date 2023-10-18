58a148a413e8a6e22e098befd2c89b1ca7d5b190
SWEMoHassan <q.and.d@outlook.com>
2023-10-18 11:34:30 -0700

    :

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,15 @@ char *cap_string(char *str)
 int i = 0;
 int new_word = 1;
 
-while (str[i]) {
-if (new_word && str[i] >= 'a' && str[i] <= 'z') {
+while (str[i])
+{
+if (new_word && str[i] >= 'a' && str[i] <= 'z')
+{
 str[i] = str[i] - 'a' + 'A';
+new_word = 0;
+}
+else if (new_word && str[i] >= 'A' && str[i] <= 'Z')
+{
 new_word = 0;
 }
 
