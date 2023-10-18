3cbe9d600b710ef0010d5e266dcf2e9f577aca78
SWEMoHassan <q.and.d@outlook.com>
2023-10-18 12:03:27 -0700

    :

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,19 +2,19 @@
 
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+  int n;
+  int a[5];
+  int *p;
 
-a[2] = 1024;
-p = &n;
-/*
-* New line of code:
-* Set the value at memory address (p + 5) to 98
-*/
-*(p + 5) = 98;
-/* ...so that this prints a[2] = 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * New line of code:
+   * Set the value at memory address (p + 5) to 98
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints a[2] = 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
 
