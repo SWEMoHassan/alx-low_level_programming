736d83dbb994e62d7dc96acca5eb732ae4faa790
SWEMoHassan <q.and.d@outlook.com>
2023-10-18 11:29:06 -0700

    :

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,20 @@
 char *cap_string(char *str)
 {
 int i = 0;
-int capitalize_next = 1;
+int new_word = 1;
 
-while (str[i] != '\0')
-{
-if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
-{
-str[i] -= ('a' - 'A');
-capitalize_next = 0;
+while (str[i]) {
+if (new_word && str[i] >= 'a' && str[i] <= 'z') {
+str[i] = str[i] - 'a' + 'A';
+new_word = 0;
 }
 
 if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{')
-{
-capitalize_next = 1;
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+str[i] == '}') {
+new_word = 1;
 }
 
 i++;
@@ -31,4 +29,3 @@ i++;
 
 return (str);
 }
-
