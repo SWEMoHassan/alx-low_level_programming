0bfa930f22a90d72feddd0c94e7d26a25eed5299
SWEMoHassan <q.and.d@outlook.com>
2023-10-18 11:52:26 -0700

    :

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,48 @@
 #include "main.h"
 
 /**
+ * is_separator - checks if a character is a word separator
+ * @c: input character
+ * Return: 1 if separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+char separators[] = " \t\n,;.!?\"(){}";
+int i;
+
+for (i = 0; separators[i] != '\0'; i++)
+{
+if (c == separators[i])
+{
+return (1);
+}
+}
+
+return (0);
+}
+
+/**
  * cap_string - capitalizes all words of a string
  * @str: input string
  * Return: pointer to the resulting string
  */
 char *cap_string(char *str)
 {
-int i = 0;
-int new_word = 1;
+int i;
 
-while (str[i])
+if (str[0] >= 'a' && str[0] <= 'z')
 {
-if (new_word && str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 'a' + 'A';
-new_word = 0;
-}
-else if (new_word && str[i] >= 'A' && str[i] <= 'Z')
-{
-new_word = 0;
+str[0] -= 32;
 }
 
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}') {
-new_word = 1;
+for (i = 1; str[i] != '\0'; i++)
+{
+if (is_separator(str[i - 1]) && (str[i] >= 'a' && str[i] <= 'z'))
+{
+str[i] -= 32;
 }
-
-i++;
 }
 
 return (str);
 }
+
