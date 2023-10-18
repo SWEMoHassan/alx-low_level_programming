095d8e56dc9ccb1f12ee0fbcc628739eb409ac4f
SWEMoHassan <q.and.d@outlook.com>
2023-10-18 11:16:21 -0700

    :

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: input string
+ * Return: pointer to the resulting string
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+int capitalize_next = 1;
+
+while (str[i] != '\0')
+{
+if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
+{
+str[i] -= ('a' - 'A');
+capitalize_next = 0;
+}
+
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{')
+{
+capitalize_next = 1;
+}
+
+i++;
+}
+
+return (str);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 
 int _putchar(char c);
 /* char *_strcat(char *dest, char *src); */
@@ -9,6 +10,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
+char *cap_string(char *str);
 
 #endif /* MAIN_H */
 
