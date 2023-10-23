4459ab8dd5dc87567616699f3e37318cc5843bac
SWEMoHassan <q.and.d@outlook.com>
2023-10-23 14:28:19 -0700

    :

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * sum_diagonal - calculates the sum of a diagonal in the matrix
+ * @a: pointer to the square matrix
+ * @size: size of the matrix
+ * @step: step value to traverse the diagonal
+ *
+ * Return: sum of the diagonal elements
+ */
+int sum_diagonal(int *a, int size, int step)
+{
+int i, sum = 0;
+for (i = 0; i < size; i++)
+{
+sum += *(a + i * size + (i * step));
+}
+return (sum);
+}
+
+/**
+ * print_diagsums - prints the sum of two
  * @a: pointer to the square matrix
  * @size: size of the matrix
  *
@@ -9,47 +28,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, diag1 = 0, diag2 = 0;
-for (i = 0; i < size; i++)
-{
-diag1 += *(a + i * size + i);
-diag2 += *(a + i * size + (size - 1 - i));
-}
+int diag1 = sum_diagonal(a, size, 1);
+int diag2 = sum_diagonal(a, size, -1);
 if (size > 0)
 {
-int temp = diag1;
-int divisor = 1;
-while (temp >= 10)
-{
-temp /= 10;
-divisor *= 10;
-}
-while (divisor > 0)
-{
-_putchar('0' + diag1 / divisor);
-diag1 %= divisor;
-divisor /= 10;
-}
-_putchar(',');
-temp = diag2;
-divisor = 1;
-while (temp >= 10)
-{
-temp /= 10;
-divisor *= 10;
-}
-while (divisor > 0)
-{
-_putchar('0' + diag2 / divisor);
-diag2 %= divisor;
-divisor /= 10;
-}
+printf("%d, ", diag1);
+printf("%d\n", diag2);
 }
 else
 {
-_putchar('0');
-_putchar(',');
-_putchar('0');
+printf("0, 0\n");
 }
-_putchar('\n');
 }
