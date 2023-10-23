e7b9950d9a766d7e50dcbdc603a099b463a5eba4
SWEMoHassan <q.and.d@outlook.com>
2023-10-23 07:23:02 -0700

    :

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
 /* Function prototype */
 char *_memset(char *s, char b, unsigned int n);
 
 /* Custom putchar function */
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
