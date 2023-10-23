073fbed5497aa51c0602109611e59c2a356e6fee
SWEMoHassan <q.and.d@outlook.com>
2023-10-23 07:15:03 -0700

    :

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,6 @@ char *_memset(char *s, char b, unsigned int n);
 
 /* Custom putchar function */
 int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
-
