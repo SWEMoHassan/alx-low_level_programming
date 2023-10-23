682f509da7cdf4420a7214ceca740c5f17f69146
SWEMoHassan <q.and.d@outlook.com>
2023-10-23 07:04:14 -0700

    :

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype */
+char *_memset(char *s, char b, unsigned int n);
+
+/* Custom putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
