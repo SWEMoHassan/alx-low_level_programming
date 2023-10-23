41a111554fa5364957740aa0852a31def2f4da30
SWEMoHassan <q.and.d@outlook.com>
2023-10-23 14:08:40 -0700

    :

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,9 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
 
