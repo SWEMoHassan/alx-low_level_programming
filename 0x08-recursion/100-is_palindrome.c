b4f4610d7bf5193fda6c5bb4c698ea68efa9429b
SWEMoHassan <q.and.d@outlook.com>
2023-10-25 10:57:03 -0700

    :

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen(s + 1));
+}
+
+/**
+ * is_palindrome_helper - checks if a string is a palindrome
+ * @s: input string
+ * @start: start index
+ * @end: end index
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+if (start >= end)
+return (1);
+if (s[start] != s[end])
+return (0);
+return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: input string
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+int len = _strlen(s);
+return (is_palindrome_helper(s, 0, len - 1));
+}
