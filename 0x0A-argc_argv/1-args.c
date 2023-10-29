1d7d70b689c5f3b09b103d7a66f4dbde4af14ecd
SWEMoHassan <q.and.d@outlook.com>
2023-10-28 18:02:28 -0700

    :

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,27 @@
+#include <stdio.h>
 #include "main.h"
 
-int main(int __attribute__((unused)) argc, char *argv[])
+/**
+ * count_arguments - Counts the number of command-line arguments.
+ * @argc: The number of command-line arguments.
+ * Return: The number of command-line arguments.
+ */
+int count_arguments(int argc)
 {
-int count = 0;
-while (argv[count] != NULL)
-{
-count++;
+return (argc);
 }
-_putchar(count + '0');
-_putchar('\n');
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: The array of command-line arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+int num_args;
+(void)argv;
+num_args = count_arguments(argc);
+printf("%d\n", num_args);
 return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -8,6 +8,7 @@ void _puts(char *str);
 void print_string(char *str);
 int multiply(int a, int b);
 int add_positive_numbers(int argc, char **argv);
+int count_arguments(int argc);
 int main(int argc, char **argv);
 
 #endif /* MAIN_H */
