eadbbcd0a0e6e8f7b4bd24d9808f7b6ed5569e11
SWEMoHassan <q.and.d@outlook.com>
2023-10-28 18:13:44 -0700

    :

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,22 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
 /**
- * main - Prints the name of the program, followed by a new line.
- *
- * Return: Always 0.
+ * print_program_name - Prints the program name.
+ * @argv: The array of command-line arguments.
  */
-extern char *argv[];
-extern int _putchar(char c);
-int main(void)
+void print_program_name(char **argv)
 {
-  char *program_name = argv[0];
-
-  while (*program_name != '\0')
-  {
-    _putchar(*program_name);
-    program_name++;
-  }
-
-  _putchar('\n');
-
-  return (0);
+char *program_name = argv[0];
+while (*program_name != '\0')
+{
+_putchar(*program_name);
+program_name++;
 }
-
+_putchar('\n');
+}
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+(void)argc;
+print_program_name(argv);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -9,6 +9,7 @@ void print_string(char *str);
 int multiply(int a, int b);
 int add_positive_numbers(int argc, char **argv);
 int count_arguments(int argc);
+void print_program_name(char **argv);
 int main(int argc, char **argv);
 
 #endif /* MAIN_H */
