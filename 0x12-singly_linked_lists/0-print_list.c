8c2f3c990545b05bc1d74b1e284792d684a914ae
SWEMoHassan <q.and.d@outlook.com>
2023-11-16 15:29:54 -0800

    :

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all elements of a list_t list.
+ * @h: Pointer to the head of the list
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%u] %s\n", 0, "(nil)");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
+}
---
+++ 0x12-singly_linked_lists/variadic_functions.h
@@ -0,0 +1,27 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* Function prototype */
+size_t print_list(const list_t *h);
+
+/* _putchar prototype */
+int _putchar(char c);
+
+#endif /* VARIADIC_FUNCTIONS_H */
