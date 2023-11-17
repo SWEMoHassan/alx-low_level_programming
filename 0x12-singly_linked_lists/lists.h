4fb55184458e3d36c24be679dbf6e7ac6ee48877
SWEMoHassan <q.and.d@outlook.com>
2023-11-16 16:12:15 -0800

    :

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,8 +2,6 @@
 #define LISTS_H
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,7 +19,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
-size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
 
