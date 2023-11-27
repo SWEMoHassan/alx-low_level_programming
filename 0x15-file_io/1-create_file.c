197dffbd794d48fd7c31ff859f78f3571626e53e
SWEMoHassan <q.and.d@outlook.com>
2023-11-27 13:55:00 -0800

    :

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * create_file - creates a file and writes text content into it
+ * @filename: name of the file to create
+ * @text_content: text to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, w, len = 0;
+if (!filename)
+return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (text_content)
+{
+while (text_content[len])
+len++;
+w = write(fd, text_content, len);
+if (w == -1 || w != len)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
